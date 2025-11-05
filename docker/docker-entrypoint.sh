92052d63f65f8e748f37e1c4e72b987338cdd737
nageshkatna <nagesh.katna@gmail.com>
2025-11-05 16:23:27 -0500

    Dockerizing the project

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+echo "Waiting for database at $DATABASE_HOST:$DATABASE_PORT..."
+while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
+  sleep 0.5
+done
+
+echo "Running migrations..."
+python manage.py migrate
+
+echo "Starting Gunicorn..."
+exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
